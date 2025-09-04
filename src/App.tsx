a2077557ff9124c45f20cf6b481aea6317f09958
Tom Harris <tomharris3@me.com>
2025-09-04 10:14:41 +0100

    import json and set state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import documents from './data/documents.json';
+import { useEffect, useState } from 'react';
+import type { Item } from './types/models';
+
+const typedDocs: Item[] = documents;
+
 function App() {
-  return <h1>Bright Tech Task</h1>;
+  const [docs, setDocs] = useState<Item[]>([]);
+
+  useEffect(() => {
+    setDocs(typedDocs);
+  }, []);
+
+  return (
+    <>
+      <h1>Bright Tech Task</h1>
+    </>
+  );
 }
 
 export default App;
