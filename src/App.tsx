c366a5cb1ea30d295ace9d8fb691a48a5446bcf1
Tom Harris <tomharris3@me.com>
2025-09-04 10:37:22 +0100

    get some initial data of files and folders mapped out

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,43 @@ import { useEffect, useState } from 'react';
 import type { Item } from './types/models';
 
 function App() {
-  const [docs, setDocs] = useState<Item[]>([]);
+  const [items, setItems] = useState<Item[]>([]);
 
   useEffect(() => {
-    setDocs(documents);
+    setItems(documents);
   }, []);
 
   return (
     <>
       <h1>Bright Tech Task</h1>
+      <ul>
+        {items.map((item) => {
+          if (item.type === 'folder') {
+            return (
+              <li key={item.name}>
+                <details>
+                  <summary>{item.name}</summary>
+                  {item.files.map((file) => (
+                    <>
+                      <p>{file.name}</p>
+                      <time>{file.added}</time>
+                      <p>{file.type}</p>
+                    </>
+                  ))}
+                </details>
+              </li>
+            );
+          }
+
+          return (
+            <li key={item.name}>
+              <p>{item.name}</p>
+              <time>{item.added}</time>
+              <p>{item.type}</p>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
